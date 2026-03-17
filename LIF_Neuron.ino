b624459f1b5410af7dab7bab6433c60a6ba09e7a
Mrunmayee Wankhede <mrunmayee.wankhede@gmail.com>
2026-03-17 10:11:12 -0400

    Add files via upload

---
+++ LIF_Neuron.ino
@@ -0,0 +1,67 @@
+int buttonPin = 2;
+int ledPin = 13;
+
+float membranePotential = 0.0;
+float decayRate = 0.5;
+int threshold = 2;
+
+unsigned long lastDecayTime = 0;
+int decayInterval = 500;
+
+unsigned long refractoryStart = 0;
+int refractoryPeriod = 1000;
+bool inRefractory = false;
+
+bool buttonWasPressed = false;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  //Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  // --- REFRACTORY CHECK ---
+  if (inRefractory) {
+    if (currentTime - refractoryStart >= refractoryPeriod) {
+      inRefractory = false;
+    } else {
+      return;
+    }
+  }
+
+  // --- DECAY ---
+  if (currentTime - lastDecayTime >= decayInterval) {
+    lastDecayTime = currentTime;
+    membranePotential -= decayRate;
+    if (membranePotential < 0) membranePotential = 0;
+  }
+
+  // --- BUTTON INPUT ---
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH && !buttonWasPressed) {   
+    membranePotential++;
+    buttonWasPressed = true;
+  }
+  if (buttonState == LOW) {
+    buttonWasPressed = false;
+  }
+
+  // --- FIRE ---
+  if (membranePotential >= threshold) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+
+    membranePotential = 0;
+    inRefractory = true;
+    refractoryStart = millis();
+
+    while(digitalRead(buttonPin) == LOW) {}   // wait for release
+    delay(50);
+  }
+
+  //Serial.println(membranePotential);
+}
